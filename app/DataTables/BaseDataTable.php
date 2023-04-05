d9b054580c5245afaa648aa654e44eda4ad7f122
Ahmad Afandi <ahmad.afandi85@gmail.com>
2023-04-05 14:22:43 +0700

    feat: update template

--- app/DataTables/BaseDataTable.php
+++ app/DataTables/BaseDataTable.php
@@ -7,4 +7,47 @@ use Yajra\DataTables\Services\DataTable;
 class BaseDataTable extends DataTable
 {
     protected $fastExcel = true;
+    protected $baseRoute;
+    protected $baseView;
+
+    /**
+     * Get the value of baseRoute
+     */ 
+    public function getBaseRoute()
+    {
+        return $this->baseRoute;
+    }
+
+    /**
+     * Set the value of baseRoute
+     *
+     * @return  self
+     */ 
+    public function setBaseRoute($baseRoute)
+    {
+        $this->baseRoute = $baseRoute;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of baseView
+     */ 
+    public function getBaseView()
+    {
+        return $this->baseView;
+    }
+
+    /**
+     * Set the value of baseView
+     *
+     * @return  self
+     */ 
+    public function setBaseView($baseView)
+    {
+        $this->baseView = $baseView;
+
+        return $this;
+    }
 }
+
